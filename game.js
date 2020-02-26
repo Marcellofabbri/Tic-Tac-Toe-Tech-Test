f41c469c8d0e4642c80d16dabe43e9ab5d30aff6
Marcellofabbri <marcellogfabbri@gmail.com>
2020-02-26 23:08:25 +0000

    add function to generate an arbitrary number of players

--- game.js
+++ game.js
@@ -1,5 +1,15 @@
+var { Player } = require('./player');
+
 function Game() {
   this.players = []
   }
 
 exports.Game = Game;
+
+Game.prototype.numberOfPlayers = function(totalNumberOfPlayers) {
+  for (i = 0; i < totalNumberOfPlayers; i++) {
+    var player = new Player()
+    player.number = i
+    this.players.push(player)
+  }
+}
--- game.test.js
+++ game.test.js
@@ -4,3 +4,15 @@ test('has a property that stores players', () => {
   var game = new Game()
   expect(game.players).toBeInstanceOf(Array);
 });
+
+test('can fill the players Array with the selected number of players', () => {
+  var game = new Game()
+  game.numberOfPlayers(2)
+  expect(game.players.length).toBe(2);
+});
+
+test('when players are generated they get assigned a number', () => {
+  var game = new Game()
+  game.numberOfPlayers(2)
+  expect(game.players[0].number).toBe(0);
+});
