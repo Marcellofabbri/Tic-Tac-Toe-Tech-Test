a68c562d98e4d89242bd8ea14ab813f894252d58
Marcellofabbri <marcellogfabbri@gmail.com>
2020-02-27 12:02:50 +0000

    refactor game to be defined before all tests

--- game.test.js
+++ game.test.js
@@ -1,28 +1,25 @@
 const { Game } = require('./game');
 
+var game = new Game()
+
 test('has a property that stores players', () => {
-  var game = new Game()
   expect(game.players).toBeInstanceOf(Array);
 });
 
 test('can fill the players Array with the selected number of players', () => {
-  var game = new Game()
   game.numberOfPlayers(2)
   expect(game.players).toHaveLength(2);
 });
 
 test('when players are generated they get assigned a number', () => {
-  var game = new Game()
   game.numberOfPlayers(2)
   expect(game.players[0].number).toBe(0);
 });
 
 test('has a playing board stored as a property', () => {
-  var game = new Game()
   expect(game.board).toBeInstanceOf(Array)
 });
 
 test('has a turn property to identify whose turn it is', () => {
-  var game = new Game()
   expect(game.turn).toBeDefined()
 });
